d21d630866a5f0ef4bce7d945202f5243cbf78ee
Livia Xavier <liviaxavier.co@gmail.com>
2022-02-15 17:17:57 -0300

    feat: collaborators info display

--- src/components/Collaborator/index.js
+++ src/components/Collaborator/index.js
@@ -10,11 +10,13 @@ function Collaborator({item}) {
       <p className='collaborator-name'>{item.name}</p>
       <small className='collaborator-highlight'>{item.highlight}</small>
       <small className='collaborator-state'>{item.state === 'open' ? 'Aberto' : 'Fechado'}</small>
-    </div>
-    <div className='collaborator-contacts'>
-      <p title='Ligar para 99 99999 9999'>t</p>
-      <p title='Enviar mensagem para 99 99999 9999'>m</p>
-      <p title="Enviar e-mail para email@email.com">e</p>
+      <div className='collaborator-infos'>
+      </div>
+      <div className='collaborator-contacts'>
+        <p title='Ligar para 99 99999 9999'>t</p>
+        <p title='Enviar mensagem para 99 99999 9999'>m</p>
+        <p title="Enviar e-mail para email@email.com">e</p>
+      </div>
     </div>
     <p className={`collaborator-description ${isOpen ? 'open' : ''}`}>{item.description}</p>
     <p className={`collaborator-more`} onClick={() => openDetails(!isOpen)}>{`${isOpen ? 'Esconder' : 'Saiba mais'}`}</p>
